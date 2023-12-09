d3125b3b02165623c43621248571b224fe3940ef
PatrickBett <patrickbett018@gmail.com>
2023-12-09 04:56:30 +0300

    Done

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive)
+VALUES
+(1,'Mr. Chocolate',20,'M','Red','Dark Brown',1),
+(2,'Rowdy',20,'M','Red','Dark Brown',1),
+(3,'Tabitha',20,'M','Red','Dark Brown',1),
+(4,'Sergeant Brown',20,'M','Red','Dark Brown',1),
+(5,'Melissa',20,'M','Red','Dark Brown',1),
+(6,'Grinch',20,'M','Red','Dark Brown',1),
+(7,'Wendy',20,'M','Red','Dark Brown',1),
+(8,NULL,NULL,NULL,NULL,NULL,NULL);
+
+
+
+
+
+
